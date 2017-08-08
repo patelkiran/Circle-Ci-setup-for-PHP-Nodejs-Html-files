e1b232661b977aac64ae1bcb6e4ffb96c43ad73c
patelkiran <kiran@3embed.com>
2017-08-08 17:59:25 +0530

    Create deploy_prod.sh

---
+++ deploy_prod.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+ssh -v root@192.241.215.91 << EOF
+
+echo '1. Updating sources'
+cd /var/www/html/test
+git checkout --force master
+git pull
+
+echo "2. Restart apache"
+sudo apache2ctl graceful
+
+echo 'Done!'
+
+EOF
