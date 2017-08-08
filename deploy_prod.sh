bc962f9ab76e5e8bd50a45f15b855278930a912b
patelkiran <kiran@3embed.com>
2017-08-08 19:15:47 +0530

    Update deploy_prod.sh

--- deploy_prod.sh
+++ deploy_prod.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-ssh root@192.241.215.91
+
+spawn ssh MyUserName@192.168.20.20
 expect "password"
 send "f6cac633f5426db71\r"
-
+interact
 
 echo '1. Updating sources'
 cd /var/www/html/test/test
