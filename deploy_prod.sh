d8f10bd2bf49c4a8386a12e2a461f667854ed941
patelkiran <kiran@3embed.com>
2017-08-10 16:02:10 +0530

    Update deploy_prod.sh

--- deploy_prod.sh
+++ deploy_prod.sh
@@ -5,10 +5,10 @@ echo process.env['user_name'];
 echo process.env['ip_address'];
 echo process.env['path'];
 echo 'starting now';
-sshpass -p process.env['pwd'] ssh -o StrictHostKeyChecking=no process.env['user_name']@process.env['ip_address']  << EOF
+sshpass -p f6cac633f5426db71 ssh -o StrictHostKeyChecking=no root@192.241.215.91  << EOF
 
 echo '1. Updating sources'
-cd process.env['path']
+cd /var/www/html/test/Circle-Ci-setup-for-PHP-Nodejs-Html-files
 git checkout --force master
 git pull
 
