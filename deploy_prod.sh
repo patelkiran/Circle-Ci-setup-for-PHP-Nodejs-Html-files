cc1d542e423004e073b8e9f1b5f78abe8f7528e8
patelkiran <kiran@3embed.com>
2017-10-30 14:05:41 +0530

    Update deploy_prod.sh

--- deploy_prod.sh
+++ deploy_prod.sh
@@ -4,7 +4,7 @@
 sshpass -p mhmTzVpKgbaumBgP ssh -o StrictHostKeyChecking=no root@107.170.105.134  << EOF
 
 echo '1. Updating sources'
-cd <path>
+cd /var/www/html/testphp/
 git checkout --force master
 git pull
 
