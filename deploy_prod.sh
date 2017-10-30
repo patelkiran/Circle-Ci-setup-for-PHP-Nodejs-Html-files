aba401a83e144b09b5c9115d2ed5d5686f11b3e8
patelkiran <kiran@3embed.com>
2017-10-30 14:11:13 +0530

    Update deploy_prod.sh

--- deploy_prod.sh
+++ deploy_prod.sh
@@ -4,7 +4,7 @@
 sshpass -p mhmTzVpKgbaumBgP ssh -o StrictHostKeyChecking=no root@107.170.105.134  << EOF
 
 echo '1. Updating sources'
-cd /var/www/html/testphp/
+cd /var/www/html/testphp/Circle-Ci-setup-for-PHP-Nodejs-Html-files/
 git checkout --force master
 git pull
 
